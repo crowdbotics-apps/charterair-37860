a6dc33c51b733c95b22bb553d8072e90429815b6
Crowdbotics <team@crowdbotics.com>
2022-12-10 19:03:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "charterair_37860",
+  url: "https://charterair_37860.botics.co",
+  api: "https://charterair_37860.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
